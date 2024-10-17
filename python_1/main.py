780e85dc980a8db322be422eb238e74d580a8279
rickyisking <bjb34331@gmail.com>
2024-10-17 17:01:13 -0400

    first commit

---
+++ python_1/main.py
@@ -0,0 +1,43 @@
+
+
+
+number1 = 4
+
+name = 'joe smith'
+
+temp = 55.5 
+boolean="videogames today"
+
+
+
+
+
+
+
+
+
+print(number1+2)
+price=90
+
+if price>40:
+   print('dont buy')
+else:
+    print('buy') 
+
+    print('welcome fighter')
+
+    
+
+for index in range(6):
+    #print('welcome fighter'
+    print(index)
+
+
+
+
+cond=0
+
+while cond<= 20:
+#print ('welcome fighter')
+    cond = cond + 5
+    
