de1caae4f11b43ffbe5005afed075696d049f883
rickyisking <bjb34331@gmail.com>
2025-01-30 17:49:00 -0500

    Done with c sharp

--- c#_1/Demo.cs
+++ c#_1/Demo.cs
@@ -7,11 +7,18 @@ public class Demo : MonoBehaviour
     // Start is called before the first frame update
   int number = 300;
   string text = "welcome";
-
+  private string secretLevel = "Top secret Level ";
+ 
+  
     // Update is called once per frame
     void Start()
     {
         Debug.Log("Welcome my son");
+        int[] numbers = {2,4,6,8};
+        List<string> Enimies = new List<string>();
+        Enimies.Add("Rex");
+        Enimies.Add("Witch");
+        Enimies.Add("Goblin");
         if (number>100){
              Debug.Log("the number is bigger than 100");
 
@@ -19,6 +26,19 @@ public class Demo : MonoBehaviour
         else{
              Debug.Log("the number is smaller than 100");
         }
+        for (int pushups = 0; pushups <= 40; pushups++)
+          {
+            Debug.Log("Pushups counter:" + pushups );
+
+           }
+       int result =  punchline(10,11);
+   
+      
        
-    }
-}  
+    }  
+   int punchline(int num1, int num2){
+
+        return num1+num2;
+   }
+   
+}   
